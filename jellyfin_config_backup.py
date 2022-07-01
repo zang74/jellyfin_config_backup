ec556dd203f29fcdcb7fcef02d1414ec5de493b3
Zang <zang74@gmail.com>
2022-07-01 12:11:27 -0400

    Update jellyfin_config_backup.py

--- jellyfin_config_backup.py
+++ jellyfin_config_backup.py
@@ -13,12 +13,12 @@ version = "1.0b"
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-v", "--version", dest="getversion", help="Gives the current version of the script.", default=False, action='store_true')
+parser.add_argument("-p", "--datapath", dest="datapath",  help="Jellyfin's data container path.", default="/volume1/docker/jellyfin")
 parser.add_argument("-d", "--destination", dest="backupdest", help="Destination folder for backups.")
 parser.add_argument("-k", "--keep", dest="keepbackups", help="Number of saved archives. If you are backing up metadata (see below) it's recommended to keep this number relatively low to avoid filling up your Synology with unnecessarily large files. The default is five.", type=int, default=5)
-parser.add_argument("-p", "--datapath", dest="datapath",  help="Jellyfin's data container path.", default="/volume1/docker/jellyfin")
 parser.add_argument("-o", "--optionalfile",  dest="optionalfile", help="Additional file(s) to include. Can be repeated once per file.", action='append')
-parser.add_argument("-m", "--metadata", dest="metadata", help="This will also back up the Jellyfin metadata folder and is off by default. Use this option with caution, as including metadata can be both slow and will dramatically increase the size of the backup files. A better, faster option is storing metadata with the files in the form of .NFO and associated image files.", default=False, action='store_true')
-parser.add_argument("-l", "--logfile", dest="optlogfileloc", help="/path/to/logfile. Optionally change this if you want logs in a more easily accessible location, such as on one of the NAS' SMB shares. The default save location is /var/log/jellyfinbackup.log")
+parser.add_argument("-m", "--metadata", dest="metadata", help="This will also back up the Jellyfin metadata folder and is off by default. Use this option with caution, as including metadata can be both slow and will dramatically increase the size of the backup files. A better, faster option is storing metadata alongside media files in the form of .NFO and associated image files and doing regular incremental backups of your media file directories.", default=False, action='store_true')
+parser.add_argument("-l", "--logfile", dest="optlogfileloc", help="/path/to/logfile/directory. Optionally change this if you want logs in a more easily accessible location, such as on one of the NAS' SMB shares. File will be named jellyfin_config_backup.log. The default save location is within /var/log/")
 parser.add_argument("-i", "--ignoredevicehash", dest="devicehash", help="Skip backing up the device.txt hash file. This might be useful if moving the config from one machine to another.", default=True, action='store_false')
 
 args = parser.parse_args()
